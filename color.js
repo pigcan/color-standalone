290b98aae35ed44dafdc206384c6a4ac6cbbc824
afc163 <afc163@gmail.com>
2016-09-10 19:06:42 +0800

    fix

--- color.js
+++ color.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["color"] = factory();
+		exports["Color"] = factory();
 	else
-		root["color"] = factory();
+		root["Color"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
